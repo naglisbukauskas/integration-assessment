e73e1bd429c0692bb3b99a0837b3eb1f16280c86
Naglis Bukauskas <71148935+naglisbukauskas@users.noreply.github.com>
2023-01-25 16:38:41 -0500

    search

--- do-lookup.js
+++ do-lookup.js
@@ -6,11 +6,95 @@
  * parameter looks like.
  * @returns {Promise<*[]>} - An array of results objects (See README.md for the full output specification).
  */
+
+const axios = require("axios");
+const baseUrl = "https://internetdb.shodan.io/";
+
 async function doLookup(entities) {
+  // Add additional logic here that will query the Shodan InternetDB API
+  // and add the results to the `lookupResults` array.
+  // Please see the README.md for full instructions
+
     const lookupResults = [];
-    // Add additional logic here that will query the Shodan InternetDB API
-    // and add the results to the `lookupResults` array.
-    // Please see the README.md for full instructions
+
+    //handle duplicates and nonIps ipvalidation?
+   entities = entities.filter((item) => (item.isIP));
+   let cleanEntities = new Set(entities.map(item => item.value));
+
+
+   //Iterative
+
+   // for await (let ip of cleanEntities) {
+   //   let requestUrl = baseUrl + ip;
+   //
+   //   //this is assumed to be true bc of the way I filtered and there being no IPv6 in the input. How verify?
+   //   let entityObj = {
+   //     value: ip,
+   //     type: 'IPv4',
+   //     isIP: true,
+   //     isDomain: false
+   //   }
+   //
+   //   let lookupResultsObject= {}
+   //
+   //   try {
+   //     let result = await axios.get(requestUrl);
+   //
+   //     lookupResultsObject.entity = entityObj;
+   //     lookupResultsObject.data = result.data;
+   //
+   //     lookupResults.push(lookupResultsObject);
+   //
+   //   } catch (e) {
+   //     lookupResultsObject.entity = entityObj;
+   //     lookupResultsObject.data = null;
+   //   }
+   // }
+   //Finished this in 20 mins how better
+
+    //This is bad but map can only be on arrays
+    cleanEntities = Array.from(cleanEntities);
+
+    let resList = await Promise.all(
+      cleanEntities.map(async ip => {
+
+        let entityObj = {
+              value: ip,
+              type: 'IPv4',
+              isIP: true,
+              isDomain: false
+            }
+
+        let requestUrl = baseUrl + ip;
+
+        try {
+
+          let apiResult = (await (axios.get(requestUrl))).data;
+
+          return {
+            entity: entityObj,
+            data: apiResult
+          }
+
+        } catch (e) {
+
+          return {
+            entity: entityObj,
+            data: null
+          }
+        }
+      })
+    )
+  //done 35:09
+
+    // lookupResults.push(resList);
+  for(let item of resList) {
+    //cant reassign bc const
+    lookupResults.push(item);
+  }
+
+
+  //stop work 40
     return lookupResults;
 }
 
